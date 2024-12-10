69001cafc90fa6917714bc9e2da5643d8cd54125
Turbok408 <edwardwildman@outlook.com>
2024-12-10 02:10:42 +0000

    commit

--- cw22.py
+++ cw22.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
 def L(t,y,y_dot,alpha,beta):
     return alpha*y_dot**2 + beta*(t**2 - 1)* y_dot **3 - y
 
@@ -19,7 +21,7 @@ def solve_ivp(alpha,beta,y_dot_0,y_0,h):
         d2ldydy_dot = (L(t[i], y[i] + h, y_dot[i] + h, alpha, beta) - L(t[i], y[i] - h, y_dot[i] + h, alpha, beta) - L(t[i], y[i] + h, y_dot[i] - h, alpha, beta) + L(t[i], y[i] - h, y_dot[i] - h, alpha, beta)) / (4 * h ** 2)
         y[i + 1] = (dLdy * h ** 2 - d2dtdy_dot * h ** 2 + d2ldydy_dot * h * y[i] - d2ld2y_dot * y[i - 1] + 2 * d2ld2y_dot * y[i]) / (d2ldydy_dot * h + d2ld2y_dot)
         y_dot[i + 1] = (y[i + 1] - y[i]) / h
-    return t,y
+    return np.delete(t,0),np.delete(y,0)
 
 def shoot(target,error,alpha,beta,h):
     guess = 10
@@ -37,22 +39,27 @@ def shoot(target,error,alpha,beta,h):
     print((result-target)/target)
     return guess
 
-h=0.0001
-y_dot_0 = shoot(0.9,0.000000001,5,5,h)
-t,y = solve_ivp(5,5,y_dot_0,1,h)
-plt.plot(t, y, label="y(t)")
-plt.xlabel("Time t")
+
+for i in range(1,8):
+    y_dot_0 = shoot(0.9,0.000000001,5,5,(1/3**i))
+    t,y = solve_ivp(5,5,y_dot_0,1,(1/3**i))
+    plt.plot(t, y, label="h = "+str((1/4**i)))
+plt.xlabel("t")
 plt.ylabel("y(t)")
-plt.title("Finite Difference Solution")
+plt.title("Finite Difference Solution For α = 5 = β with Different h Values")
 plt.legend()
 plt.grid()
 plt.show()
-y_dot_0 = shoot(0.9,0.000000001,7/4,5,h)
-t,y = solve_ivp(7/4,5,y_dot_0,1,h)
-plt.plot(t, y, label="y(t)")
-plt.xlabel("Time t")
+y_dot_0 = shoot(0.9,0.000000001,7/4,5,0.15)
+t,y = solve_ivp(5, 5, y_dot_0, 1, 0.15)
+plt.plot(t, y, label="h = 0.15")
+for i in range(2,8):
+    y_dot_0 = shoot(0.9,0.000000001,7/4,5,(1/3**i))
+    t,y = solve_ivp(5,5,y_dot_0,1,(1/3**i))
+    plt.plot(t, y, label="h = "+str((1/3**i)))
+plt.xlabel("t")
 plt.ylabel("y(t)")
-plt.title("Finite Difference Solution 2")
+plt.title("Finite Difference Solution For α = 5, β=7/4 with Different h Values")
 plt.legend()
 plt.grid()
 plt.show()
